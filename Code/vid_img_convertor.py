f2847e095da316825be755117b1f24cb0a8a323e
Argha Mukherjee <arghamukherjee04@gmail.com>
2022-03-05 17:15:14 +0530

    Video_TO_Image_Extraction_CV2

---
+++ Code/vid_img_convertor.py
@@ -0,0 +1,41 @@
+import cv2
+import os
+
+video_pth = "/Users/arghamukherjee/Downloads/Codebase/GithbProjects/VideoToImgConvertorPython/source_video/running_with_a_kite.mp4"
+
+data = cv2.VideoCapture(video_pth)
+try:
+
+    # creating a folder named data
+    if not os.path.exists('Data'):
+        os.makedirs('Data')
+
+# if not created then raise error
+except OSError:
+    print('Error: Creating directory of data')
+
+# frame
+currentframe = 0
+
+while (True):
+
+    # reading from frame
+    ret, frame = data.read()
+
+    if ret:
+        # if video is still left continue creating images
+        name = './source_video/frame' + str(currentframe) + '.jpg'
+        print('Creating...' + name)
+
+        # writing the extracted images
+        cv2.imwrite(name, frame)
+
+        # increasing counter so that it will
+        # show how many frames are created
+        currentframe += 1
+    else:
+        break
+
+# Release all space and windows once done
+data.release()
+cv2.destroyAllWindows()
